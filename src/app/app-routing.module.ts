cb822efd7fcfcb64537a7a9481d771a29ff62380
Jorge <jorge.garcia@ext.docout.es>
2018-06-26 09:32:05 +0200

    Primera configuración

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
+import { AuthGuardService } from './core/services/auth-guard.service';
+import { DialogoComponent } from './bootstrap/dialogo/dialogo.component';
+import { DireccionComponent } from './bootstrap/direccion/direccion.component';
+import { HeroFormComponent } from './formularios/hero-form/hero-form.component';
+import { HeroDetailComponent } from './formularios/hero-detail/hero-detail.component';
+import { HeroListComponent } from './formularios/hero-detail/hero-list/hero-list.component';
+import { AccordionExampleComponent } from './bootstrap/accordion-example/accordion-example.component';
+import { AlertsExampleComponent } from './bootstrap/alerts-example/alerts-example.component';
+import { ButtonsComponent } from './bootstrap/buttons/buttons.component';
+import { CarouselComponent } from 'ngx-bootstrap';
+import { CollapseComponent } from './bootstrap/collapse/collapse.component';
+
+const routes: Routes = [
+    { path: 'login', loadChildren: './login/login.module#LoginModule' },
+    { path: 'direccion', component: DireccionComponent },
+    { path: 'heroForm', component: HeroFormComponent },
+    { path: 'heroDetail', component: HeroDetailComponent },
+    { path: 'heroList', component: HeroListComponent },
+    { path: 'accordion', component: AccordionExampleComponent },
+    { path: 'alert', component: AlertsExampleComponent },
+    { path: 'buttons', component: ButtonsComponent },
+    { path: 'carousel', component: CarouselComponent },
+    { path: 'collapse', component: CollapseComponent },
+    { path: '', children: [], canActivate: [AuthGuardService] }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { DialogoComponent } from './bootstrap/dialogo/dialogo.component';
+import { DireccionComponent } from './bootstrap/direccion/direccion.component';
+import { CoreModule } from './core/core.module';
+import { HeroFormComponent } from './formularios/hero-form/hero-form.component';
+import { HeroDetailComponent } from './formularios/hero-detail/hero-detail.component';
+import { HeroListComponent } from './formularios/hero-detail/hero-list/hero-list.component';
+import { HeroService } from './core/services/hero.service';
+import { BsModalService, ModalModule, AccordionModule, AlertModule, ButtonsModule, CarouselModule, CollapseModule } from 'ngx-bootstrap';
+import { AccordionExampleComponent } from './bootstrap/accordion-example/accordion-example.component';
+import { AlertsExampleComponent } from './bootstrap/alerts-example/alerts-example.component';
+import { ButtonsComponent } from './bootstrap/buttons/buttons.component';
+import { CarouselComponent } from './bootstrap/carousel/carousel.component';
+import { CollapseComponent } from './bootstrap/collapse/collapse.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogoComponent,
+    DireccionComponent,
+    HeroFormComponent,
+    HeroDetailComponent,
+    HeroListComponent,
+    AccordionExampleComponent,
+    AlertsExampleComponent,
+    ButtonsComponent,
+    CarouselComponent,
+    CollapseComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AccordionModule.forRoot(),
+    AlertModule.forRoot(),
+    ModalModule.forRoot(),
+    ButtonsModule.forRoot(),
+    CarouselModule.forRoot(),
+    CollapseModule.forRoot()
   ],
-  providers: [],
+  entryComponents: [DialogoComponent],
+  providers: [BsModalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
