8f48be0375ba6fc4793e80a69ecdeaea0d143ef4
Jorge <jorge.garcia@ext.docout.es>
2018-07-11 13:11:49 +0200

    Reestructuración de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login/login.component';
 import { AuthGuardService } from './core/services/auth-guard.service';
-import { DialogoComponent } from './bootstrap/dialogo/dialogo.component';
-import { DireccionComponent } from './bootstrap/direccion/direccion.component';
 import { HeroFormComponent } from './formularios/hero-form/hero-form.component';
 import { HeroDetailComponent } from './formularios/hero-detail/hero-detail.component';
 import { HeroListComponent } from './formularios/hero-detail/hero-list/hero-list.component';
-import { AccordionExampleComponent } from './bootstrap/accordion-example/accordion-example.component';
-import { AlertsExampleComponent } from './bootstrap/alerts-example/alerts-example.component';
-import { ButtonsComponent } from './bootstrap/buttons/buttons.component';
-import { CarouselComponent } from 'ngx-bootstrap';
-import { CollapseComponent } from './bootstrap/collapse/collapse.component';
-import { DatepickerComponent } from './bootstrap/datepicker/datepicker.component';
-import { DropdownsComponent } from './bootstrap/dropdowns/dropdowns.component';
-import { ModalsComponent } from './bootstrap/modals/modals.component';
-import { PaginationComponent } from './bootstrap/pagination/pagination.component';
-import { TimepickerComponent } from './bootstrap/timepicker/timepicker.component';
-import { TooltipComponent } from './bootstrap/tooltip/tooltip.component';
-import { PopoverComponent } from './bootstrap/popover/popover.component';
 
 const routes: Routes = [
     { path: 'login', loadChildren: './login/login.module#LoginModule' },
-    { path: 'direccion', component: DireccionComponent },
+    { path: 'bootstrap', loadChildren: './bootstrap/bootstrap.module#BootstrapModule' },
     { path: 'heroForm', component: HeroFormComponent },
     { path: 'heroDetail', component: HeroDetailComponent },
     { path: 'heroList', component: HeroListComponent },
-    { path: 'accordion', component: AccordionExampleComponent },
-    { path: 'alert', component: AlertsExampleComponent },
-    { path: 'buttons', component: ButtonsComponent },
-    { path: 'carousel', component: CarouselComponent },
-    { path: 'collapse', component: CollapseComponent },
-    { path: 'datepicker', component: DatepickerComponent },
-    { path: 'dropdown', component: DropdownsComponent },
-    { path: 'modal', component: ModalsComponent },
-    { path: 'pagination', component: PaginationComponent },
-    { path: 'popover', component: PopoverComponent },
-    { path: 'time', component: TimepickerComponent },
-    { path: 'tooltip', component: TooltipComponent },
     { path: '', children: [], canActivate: [AuthGuardService] }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,52 +6,21 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { DialogoComponent } from './bootstrap/dialogo/dialogo.component';
-import { DireccionComponent } from './bootstrap/direccion/direccion.component';
 import { CoreModule } from './core/core.module';
 import { HeroFormComponent } from './formularios/hero-form/hero-form.component';
 import { HeroDetailComponent } from './formularios/hero-detail/hero-detail.component';
 import { HeroListComponent } from './formularios/hero-detail/hero-list/hero-list.component';
 import { HeroService } from './core/services/hero.service';
-import {
-  BsModalService, ModalModule, AccordionModule, AlertModule, ButtonsModule, CarouselModule, CollapseModule,
-  BsDatepickerModule, BsDropdownModule, PaginationModule, TimepickerModule, TooltipModule, PopoverModule
-} from 'ngx-bootstrap';
-import { AccordionExampleComponent } from './bootstrap/accordion-example/accordion-example.component';
-import { AlertsExampleComponent } from './bootstrap/alerts-example/alerts-example.component';
-import { ButtonsComponent } from './bootstrap/buttons/buttons.component';
-import { CarouselComponent } from './bootstrap/carousel/carousel.component';
-import { CollapseComponent } from './bootstrap/collapse/collapse.component';
-import { DatepickerComponent } from './bootstrap/datepicker/datepicker.component';
-import { DropdownsComponent } from './bootstrap/dropdowns/dropdowns.component';
-import { ModalsComponent } from './bootstrap/modals/modals.component';
-import { PaginationComponent } from './bootstrap/pagination/pagination.component';
-import { TimepickerComponent } from './bootstrap/timepicker/timepicker.component';
-import { TooltipComponent } from './bootstrap/tooltip/tooltip.component';
-import { PopoverComponent } from './bootstrap/popover/popover.component';
+import { BsModalService } from 'ngx-bootstrap';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DialogoComponent,
-    DireccionComponent,
     HeroFormComponent,
     HeroDetailComponent,
-    HeroListComponent,
-    AccordionExampleComponent,
-    AlertsExampleComponent,
-    ButtonsComponent,
-    CarouselComponent,
-    CollapseComponent,
-    DatepickerComponent,
-    DropdownsComponent,
-    ModalsComponent,
-    PaginationComponent,
-    TimepickerComponent,
-    TooltipComponent,
-    PopoverComponent
+    HeroListComponent
   ],
   imports: [
     HttpClientModule,
@@ -59,21 +28,8 @@ import { PopoverComponent } from './bootstrap/popover/popover.component';
     AppRoutingModule,
     CoreModule,
     FormsModule,
-    ReactiveFormsModule,
-    AccordionModule.forRoot(),
-    AlertModule.forRoot(),
-    ModalModule.forRoot(),
-    ButtonsModule.forRoot(),
-    CarouselModule.forRoot(),
-    CollapseModule.forRoot(),
-    BsDatepickerModule.forRoot(),
-    BsDropdownModule.forRoot(),
-    PaginationModule.forRoot(),
-    PopoverModule.forRoot(),
-    TimepickerModule.forRoot(),
-    TooltipModule.forRoot()
+    ReactiveFormsModule
   ],
-  entryComponents: [DialogoComponent],
   providers: [BsModalService],
   bootstrap: [AppComponent]
 })
